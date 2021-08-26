02f20ac8d9c8d6a1c436beeaece86967279622ea
parmarkuldeeep <81968141+parmarkuldeeep@users.noreply.github.com>
2021-08-26 11:26:41 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,25 @@
-var firstNumber = prompt("provide your first number");
-var SecondNumber = prompt("porvide your second number"); 
-alert(Number(firstNumber) + Number(SecondNumber));
+
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the result
+alert(`${number1} ${operator} ${number2} = ${result}`);
